cad2ef78e907963ff35ea6bcdb3373d4879cc041
lastminutengineer <yeshwantgkv@gmail.com>
2026-01-25 18:00:55 +0530

    dsa pattern2

---
+++ arrraysdsa/twopointerpattern.cpp/twosum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[] = {1, 2, 3, 4, 6, 8};
+    int n  = sizeof(arr) / sizeof(arr[0]);
+    int target = 10;
+    int i = 0;
+    int j= n-1;
+    while (i < j){
+        int sum = arr[i] + arr[j];
+    
+    if (sum == target){
+ 
+        cout << "FOUND" << " " <<  arr[i] << "+" << arr[j]<< " = " << target << endl;
+        return 0;
+    }
+
+    else if (sum < target){
+           i++ ;
+    }
+    else {
+        j-- ;
+    }
+    }
+    cout << "not found" << endl ;
+    return 0;
+}
