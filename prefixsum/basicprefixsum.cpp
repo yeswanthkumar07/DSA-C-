ff7c5f970fd934044d076058a172800b43f55d69
lastminutengineer <yeshwantgkv@gmail.com>
2026-02-10 19:14:36 +0530

    prefix sum

---
+++ prefixsum/basicprefixsum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int arr[] = {3,7,2,5,8};
+    int n = 5;
+    int prefix[n];
+    prefix[0] = arr[0];
+    for (int i =1 ; i < n ; i++){
+        
+        prefix[i] = arr[i - 1] + arr[i];
+
+    }
+    for (int i = 0 ; i <n ; i++){
+        cout << prefix[i] << endl;
+    }
+    return 0;
+}
