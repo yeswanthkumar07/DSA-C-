36bb340eafdb7a3d3e1bdb23b7adb8d869aa4ffc
lastminutengineer <yeshwantgkv@gmail.com>
2026-03-22 12:35:12 +0530

    corrected headers

--- vectors/sortedvector.cpp
+++ vectors/sortedvector.cpp
@@ -1,18 +1,27 @@
-#include "iostream";
+#include <iostream>
 #include <functional>
 #include <algorithm>
-#include "vector";
-using namespace std;;
-int main(){
-    vector <int> v = {10,30,50,60,80,9,54,63};
-    // ascending order
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> v = {10, 30, 50, 60, 80, 9, 54, 63};
+
+    // Ascending order
     sort(v.begin(), v.end());
-    for (int i = 0; i  < v.size();i++){
+
+    for (int i = 0; i < v.size(); i++) {
         cout << v[i] << endl;
     }
-      /* dscending order
+
+    /*
+    // Descending order
     sort(v.begin(), v.end(), greater<int>());
-    for (int i = 0; i  < v.size();i++){
+    for (int i = 0; i < v.size(); i++) {
         cout << v[i] << endl;
-    } */
+    }
+    */
+
+    return 0;
 }
