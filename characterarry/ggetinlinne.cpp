2667cbd797128d898d498a8ec37137d53b4a7907
lastminutengineer <yeshwantgkv@gmail.com>
2026-03-25 16:39:19 +0530

    inbuildfunctions

---
+++ characterarry/ggetinlinne.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    cout << "enter input" << endl;
+    char name[100];
+    cin.getline(name, 100);
+    cout << "welcome mr :" << name << endl;
+}
