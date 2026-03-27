e401d4f6231553ad6a0c6808d71a7a2c14ee38b2
lastminutengineer <yeshwantgkv@gmail.com>
2026-03-28 00:17:01 +0530

    prntprefixs

---
+++ characterarry/inbuildfunctions/prntprefixes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace  std;
+    void printprefix(char str[]){
+        for (int i = 0; str[i] != '\0'; i++){
+            for (int j = 0; j <= i; j--){
+                cout << str[j];
+            }
+            cout << endl;
+        }
+    }
+int main(){
+    char str[] = "yashwanth kumar rambhaaaa";
+    printprefix(str);
+    return 0;
+}
