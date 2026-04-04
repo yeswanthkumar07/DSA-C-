d6a2e20c2ca08274e6250cd20bd7c7914bec0d04
lastminutengineer <yeshwantgkv@gmail.com>
2026-04-04 21:39:53 +0530

    commit3

--- arrraysdsa/simpletraversal/arraydsa.cpp
+++ arrraysdsa/simpletraversal/arraydsa.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int marks[5] = {20,30,52,56,89};
+    int marks[6] = {20,30,52,56,89,67};
    int sum = 0; 
     for (int i = 0 ; i < 5;i++){
         sum = sum + marks[i];
